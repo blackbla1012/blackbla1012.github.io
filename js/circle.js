c35bb216253d0596494186695bb30b02fa42a582
blackbla1012 <65478961+blackbla1012@users.noreply.github.com>
2024-03-14 21:30:54 -0400

    udpate cursor

--- js/circle.js
+++ js/circle.js
@@ -1,5 +1,13 @@
 var circle = document.querySelector(".circle");
-document.addEventListener("mousemove", function(e){
-    circle.style.top = e.pageY + "px";
-    circle.style.left = e.pageX + "px";
+
+// var link = document.querySelectorAll('.MainImg');
+
+document.addEventListener('mousemove', (e) => {
+    const height = circle.offsetHeight;
+    const width = circle.offsetWidth;
+    
+    setTimeout(() => {
+        circle.style.top = (e.pageY - height / 2) + "px";
+        circle.style.left = (e.pageX - width / 2) + "px";
+    }, 20);
 })
